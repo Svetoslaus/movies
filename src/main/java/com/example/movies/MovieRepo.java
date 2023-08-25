d873efe053b8d9ee6090fcb2779144ed9aede181
Svetoslaus <svetoslav.shtereff@gmail.com>
2023-08-25 23:10:01 +0200

    third commit; complete the backendside

--- src/main/java/com/example/movies/MovieRepo.java
+++ src/main/java/com/example/movies/MovieRepo.java
@@ -5,6 +5,10 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.Optional;
+
 @Repository
 public interface MovieRepo extends MongoRepository<Movie, ObjectId> {
+
+    Optional<Movie> findMovieByImdbId(String imdbId);
 }
--- src/main/java/com/example/movies/MovieService.java
+++ src/main/java/com/example/movies/MovieService.java
@@ -1,9 +1,11 @@
 package com.example.movies;
 
+import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class MovieService {
@@ -12,5 +14,8 @@ public class MovieService {
 
     public List<Movie> allMovies(){
         return movieRepo.findAll();
+    }
+    public Optional<Movie> singleMovie(String imdbId){
+        return movieRepo.findMovieByImdbId(imdbId);
     }
 }
